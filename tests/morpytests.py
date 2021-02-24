c1c497f021b7c33ca7b4eee7a425b42ebc6f10b2
Ray Lemon <big0424@gmail.com>
2021-02-24 18:59:42 +0100

    Retour en arrière pour refaire des tests

--- tests/morpytests.py
+++ tests/morpytests.py
@@ -59,34 +59,3 @@ predic_random = [["X", "O", "_"],
                  ["X", "O", "_"],
                  ["O", "X", "_"]]
 
-
-def test_is_won_vert_x():
-    assert morpy.is_vertical(win_board_x_ver, "X") is True
-
-
-def test_is_won_vert_o():
-    assert morpy.is_vertical(win_board_o_ver, "O") is True
-
-
-def test_is_lost_ver_x():
-    assert morpy.is_vertical(win_board_x_ver, "O") is False
-
-
-def test_is_lost_ver_o():
-    assert morpy.is_vertical(win_board_o_ver, "X") is False
-
-
-def test_win_is_slash_x():
-    assert morpy.is_slash(win_board_x_slash, "X") is True
-
-
-def test_win_is_slash_o():
-    assert morpy.is_slash(win_board_o_slash, "O") is True
-
-
-def test_win_is_backslash_x():
-    assert morpy.is_backslash(win_board_x_backslash, "X") is True
-
-
-def test_win_is_backslash_o():
-    assert morpy.is_backslash(win_board_o_backslash, "O") is True
